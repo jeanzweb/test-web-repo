7143927ad0b1f6b94d6c8538231c2f111a7909e8
jeanzweb <jeanz@gmx.net>
2017-03-02 10:58:46 +0100

    Update MyJava.java

--- java/MyJava.java
+++ java/MyJava.java
@@ -1,7 +1,8 @@
 public class MyJava {
 
   public static void main(Strin[] args) {
-    System.out.println("Hello");
+    System.out.println("Hello-1");
+    System.out.println("Hello-2");
   }
 
 }
